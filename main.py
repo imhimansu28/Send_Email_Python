93bc6cd2312fafe945a3279b83bfcab028d1068d
Himanshu Gupta <57537963+imhimansu28@users.noreply.github.com>
2020-03-07 15:40:03 +0530

    Some changes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pathlib import Path
 html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Himanshu Gupta'
-email['to'] = 'imhimansu28@gmail.com'
+email['to'] = 'example@gmail.com'
 email['subject'] = 'You won 1,000,000 dollars!'
 
 email.set_content(html.substitute({'name':'TimTin'}), 'html')
@@ -14,6 +14,6 @@ email.set_content(html.substitute({'name':'TimTin'}), 'html')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp :
     smtp.ehlo()
     smtp.starttls( )
-    smtp.login('webdevhimanshu@gmail.com', 'sxyklceinziaegys')
+    smtp.login('demo@gmail.com', '***********')
     smtp.send_message(email)
     print('all good boss! ')
